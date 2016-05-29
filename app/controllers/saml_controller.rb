f4626647edc254ad86be6c8fe95cec116263c8a4
chcholman <caitlin.holman@gmail.com>
2016-05-29 17:09:39 -0400

    Add redirect for saml

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -18,7 +18,7 @@ class SamlController < ApplicationController
         auto_login @user
         User.increment_counter(:visit_count, @user.id)
         session[:course_id] = CourseRouter.current_course_for @user
-        respond_with @user, location: dashboard_path
+        redirect_back_or_to dashboard_path
       else
         redirect_to um_pilot_path
       end
